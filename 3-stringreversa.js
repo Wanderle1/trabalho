71cda6a2cad1341374921610bc4816d0915f6f97
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-07-29 14:45:34 -0300

    Update 3-stringreversa.js

--- 3-stringreversa.js
+++ 3-stringreversa.js
@@ -8,7 +8,7 @@ para dividir a string em um array de caracteres, reverse() para
 inverter o array e join('') para juntar os caracteres de volta em uma string.
 
 Programa desenvolvido por:
- - Aluno 1
+ - Wanderlei
  - Aluno 2
  - Aluno 3
  - Aluno 4
